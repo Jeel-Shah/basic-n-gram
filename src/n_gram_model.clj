e1bf8b81d23abdd53085ba6e8353bbb3ff424649
Jeel Shah <jeelshah96@gmail.com>
2016-04-30 10:58:45 -0400

    Counting frequencies

--- src/n_gram_model.clj
+++ src/n_gram_model.clj
@@ -1,6 +1,15 @@
 (ns n-gram-model)
 
+(require '[clojure.string :as str])
+
 ;; Let's grab the user input
-(def user-input (readline))
+(def user-input (read-line))
 
 (println user-input)
+
+;; Split and count the occurences of each word and figure out the total as well
+(defn count-and-split [lines]
+  (frequencies (str/split lines #" ")))
+
+
+(println (count-and-split "Wait isn't this just great I mean what do you think?"))
