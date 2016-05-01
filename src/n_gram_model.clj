0a63b7fa1bf028a446d7949dbb395c7da2ded7b5
Jeel Shah <jeelshah96@gmail.com>
2016-04-30 22:03:20 -0400

    Calculates the bigram prob.

--- src/n_gram_model.clj
+++ src/n_gram_model.clj
@@ -2,14 +2,35 @@
 
 (require '[clojure.string :as str])
 
-;; Let's grab the user input
-(def user-input (read-line))
-
-(println user-input)
+(def test-string "Wait isn't this just great I mean what do you think?")
 
 ;; Split and count the occurences of each word and figure out the total as well
-(defn count-and-split [lines]
-  (frequencies (str/split lines #" ")))
+
+(defn string-split [my-string]
+  (str/split my-string #" "))
+
+(defn sum [list-of-values]
+  (apply + list-of-values))
+
+(defn select-values [map keys]
+  (vals (select-keys map keys)))
+
+(defn take-two [my-string]
+  (take-last 2 my-string))
+
+(defn string-tuple [my-string]
+  (partition 2 1 my-string))
+
+(defn split-and-frequent-tuple [lines]
+  (frequencies (string-tuple lines)))
+
+(defn split-and-frequent [lines]
+  (frequencies lines))
 
 
-(println (count-and-split "Wait isn't this just great I mean what do you think?"))
+(def string-pairs (split-and-frequent-tuple (string-split "<s> I am Sam </s> <s> Sam I am </s> <s> I do not like green eggs and ham </s>")))
+(def word-freq(split-and-frequent (string-split "<s> I am Sam </s> <s> Sam I am </s> <s> I do not like green eggs and ham </s>")))
+
+;; Calculate that bigram probability
+(println (/ (string-pairs ["<s>" "I"])
+            (word-freq "<s>")))
